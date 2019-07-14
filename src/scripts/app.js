36c5f3317401507313b3711e22d3989b5cf95492
Marek Bojec <marek.bojec@gmail.com>
2019-07-14 09:19:26 +0200

    creating and styling master page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,13 +2,17 @@ require('../resources/style/main.scss');
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import {Navigation} from './navigation'
+import {Master} from './master'
 
 class App extends Component {render() {
   return (
     <div className={'main'}>
       <div className={'row'}>
-        <div className={'col-xs-12 col-md-2 col-lg-1'}>
+        <div className={'col-xs-12 col-md-1'}>
           <Navigation/>
+        </div>
+        <div className={'col-xs-12 col-md-4 col-xl-3'}>
+          <Master/>
         </div>
       </div>
     </div>
---
+++ src/scripts/master.js
@@ -0,0 +1,36 @@
+require('../resources/style/main.scss');
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+class Master extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      display_selected: 'overall'
+    }
+  }
+
+  setDisplayOptionSelected = (name) => {
+    this.setState({
+      display_selected: name,
+    })
+  };
+
+  render() {
+    return(
+        <div className={'master'}>
+          <div className={'decoration--top'}/>
+          <div className={'row master__button__group'}>
+            <div className={'col-xs-2 col-sm-3'}/>
+            <button className={`col-xs-4 col-sm-3 master__button__group__button master__button__group__button--master ${this.state.display_selected === 'overall'? "master__button__group__button--selected": "master__button__group__button--unselected"}`} onClick={event => this.setDisplayOptionSelected('overall')}>Overall</button>
+            <button className={`col-xs-4 col-sm-3 master__button__group__button master__button__group__button--detail ${this.state.display_selected === 'detail'? "master__button__group__button--selected": "master__button__group__button--unselected"}`} onClick={event => this.setDisplayOptionSelected('detail')}>Detail</button>
+            <div className={'col-xs-2 col-sm-3'}/>
+          </div>
+          <div className={'decoration--bottom'}/>
+        </div>
+    )
+  }
+}
+
+export {Master}
