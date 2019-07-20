7008be60443d026720b43c821883e4e948ae7cc5
Marek Bojec <marek.bojec@gmail.com>
2019-07-20 22:26:09 +0200

    deleting search page

--- src/scripts/routes.js
+++ src/scripts/routes.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Route, HashRouter, Switch } from 'react-router-dom'
 import {Main} from "./pages/Main";
 import {NotFound} from "./pages/NotFound";
-import {Search} from "./pages/Search";
 
 export default props => (
   <HashRouter>
       <Switch>
         <Route exact path='/' component={ Main } />
-        <Route exact path='/search' component={Search} />
         <Route path='*' component={NotFound} />
       </Switch>
   </HashRouter>
