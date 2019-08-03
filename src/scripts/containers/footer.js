c61e3e5e65467f6ed60fa3efda997d581f30d051
Marek Bojec <marek.bojec@gmail.com>
2019-08-03 22:42:03 +0200

    styling footer

--- src/scripts/containers/footer.js
+++ src/scripts/containers/footer.js
@@ -8,14 +8,19 @@ class Footer extends Component{
 
 
   render() {
+
+    const style = {
+      color: '#C7C7C7'
+    };
+
     return(
       <>
         <div className={'row footer'}>
-          <div className={'col-xs-12 col-sm-8 footer__dark_sky_info'}><a href={'https://darksky.net/poweredby/'}>Powered by Dark Sky</a></div>
-          <div className={'col-xs-12 col-sm-4 footer__contact_container'}>
-            <div>Contact</div>
-            <div className={'footer__contact_container__email'}><FontAwesomeIcon className={'footer__contact_container__email__logo'} icon={faEnvelope}/><div className={'footer__contact_container__email__address'}>mbojec.dev@gmail.com</div></div>
-            <div className={'footer__contact_container__github'}><GithubLogo/><a className={'footer__contact_container__github'} href={'https://github.com/mbojec'}>mbojec</a></div>
+          <div className={'col-xs-12 col-sm-8 footer__dark-sky-info'}><a href={'https://darksky.net/poweredby/'}>Powered by Dark Sky</a></div>
+          <div className={'col-xs-12 col-sm-4 footer__contact-container'}>
+            <div className={'footer__contact-container__label'}>Contact</div>
+            <div className={'footer__contact-container__email'}><FontAwesomeIcon className={'footer__contact-container__email__logo'} icon={faEnvelope}/><a style={style} href="mailto:mbojec.dev@gmail.com" className={'footer__contact-container__email__address'}>mbojec.dev@gmail.com</a></div>
+            <div className={'footer__contact-container__github'}><GithubLogo/><a className={'footer__contact-container__github'} href={'https://github.com/mbojec'}>mbojec</a></div>
           </div>
         </div>
       </>
